9256cecaef6027d78eae15791dbd4c8aa3bb49c5
Bilal <bilallbhatti786@gmail.com>
2022-06-28 21:14:58 +0500

    finished

--- motion_detection.py
+++ motion_detection.py
@@ -3,8 +3,9 @@ import time
 
 
 def detect_motion():
-    global frame1, frame2, main_frame
-    res = False
+    global frame1, frame2
+
+
     diff = cv2.absdiff(frame1, frame2)
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5,5), 0)
@@ -13,36 +14,27 @@ def detect_motion():
     contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
-        (x, y, w, h) = cv2.boundingRect(contour)
-
-        if cv2.contourArea(contour) <10:
-            res = True
-            print(x,y,w,h)
-            # cv2.rectangle(main_frame,(x,y),(w,h),(255,0,0),2)
-            
+        if cv2.contourArea(contour) > 100:
+            return True            
     
-    frame1 = frame2
-    _, frame2 = cap.read()
-
-    return res, frame1
+    
+    return False
 
 
 
-cap = cv2.VideoCapture('Batting1.mp4')
+cap = cv2.VideoCapture(0)
 fps= int(cap.get(cv2.CAP_PROP_FPS))
 
 _, frame1 = cap.read()
 _, frame2 = cap.read()
-_, main_frame = cap.read()
 
 while True:
     _, frame1 = cap.read()
     _, frame2 = cap.read()
-    _, main_frame = cap.read()
 
     time.sleep(1/fps)
     
-    _,framee = detect_motion()
-
-    cv2.imshow('live',main_frame)
+    res = detect_motion()
+    print(res)
+    cv2.imshow('live',frame1)
     cv2.waitKey(1)
